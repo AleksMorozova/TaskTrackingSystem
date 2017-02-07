4746d76f8db46c2ad50acced2e0ab138ad732b93
Sasha <67SASHA@ukr.net>
2017-02-07 19:58:32 +0200

    -- create master page
    -- add css files
    -- add font files
    -- create login and dashboard controllers

---
+++ TaskTrackingSystem/Client/Scripts/custom/menu.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    var menu = $("#image-menu");
+    var nav_bar = $('.navbar-nav');
+    var wrapper = $("#wrapper");
+    $(menu).click(function () {
+        if ($(menu).hasClass('active')) {
+            $(nav_bar).animate({ left: '225px' }, 200);
+            $(wrapper).animate({ 'padding-left': '225px' }, 200);
+            $(menu).removeClass('active');
+        } else {
+            $(nav_bar).animate({ left: '-225px' }, 200);
+            $(wrapper).animate({ 'padding-left': '0px' }, 200);
+            $(menu).addClass('active');
+        }
+    });
+});
